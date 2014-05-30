d57b01d4f543302e6d2a246e78c30263f61d434c
Sean Kinney <skinney@zendesk.com>
2014-05-30 12:11:10 -0500

    Committing initial version of the app

---
+++ app.js
@@ -0,0 +1,42 @@
+(function(){
+
+  return {
+    defaultState: 'loading',
+
+    requests: {
+      getUserDetails: function(email) {
+        return {
+          url: 'http://appstraining.herokuapp.com/api/users.json?email=' + email,
+          type: 'GET',
+          dataType: 'json'
+        }
+      }
+    },
+
+    events: {
+      'app.activated'       : 'activated',
+      'getUserDetails.done' : 'showUserDetails',
+      'getUserDetails.fail' : 'fail'
+    },
+
+    activated: function() {
+      this.ajax('getUserDetails', this.currentUser().email());
+    },
+
+    showUserDetails: function(users) {
+      var count = 0;
+
+      for (user in users) {
+        users[user].external_id = user;
+        count++;
+      }
+
+      this.switchTo('crm_details', {"users" : users, "count" : count});
+    },
+
+    fail: function(data) {
+      this.switchTo('fetch_fail');
+    }
+  };
+
+}());
